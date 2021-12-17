1f62de7a51276c0a0f60a691f0f64a2f626fddba
Sara <khj@exntu.com>
2021-12-17 16:42:16 +0900

    [feat] hello api param 변경

--- src/main/java/com/example/book/springboot/web/HelloController.java
+++ src/main/java/com/example/book/springboot/web/HelloController.java
@@ -1,13 +1,15 @@
 package com.example.book.springboot.web;
 
+import com.example.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "hello";
+    public HelloResponseDto hello(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
